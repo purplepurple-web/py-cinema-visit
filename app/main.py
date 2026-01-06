acdda65435f95280ba310498b087fc147cf0b8f0
“Purple” <purplepurple773@gmail.comgit config --global user.email purplepurple773@gmail.com>
2026-01-06 17:46:21 +0700

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,29 @@
-# write your imports here
+from app.people.customer import Customer
+from app.people.cinema_staff import Cleaner
+from app.cinema.bar import CinemaBar
+from app.cinema.hall import CinemaHall
 
 
 def cinema_visit(customers: list, hall_number: int, cleaner: str, movie: str):
-    # write you code here
-    pass
+    customer_objects = []
+
+    for customer in customers:
+        customer_obj = Customer(
+            name=customer["name"],
+            food=customer["food"]
+        )
+        customer_objects.append(customer_obj)
+
+        CinemaBar.sell_product(
+            product=customer_obj.food,
+            customer=customer_obj
+        )
+
+    hall = CinemaHall(number=hall_number)
+    cleaning_staff = Cleaner(name=cleaner)
+
+    hall.movie_session(
+        movie_name=movie,
+        customers=customer_objects,
+        cleaning_staff=cleaning_staff
+    )
